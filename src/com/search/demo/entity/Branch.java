e868daea2728003467ffc91eb8104d678742cba5
zhenfeii <zhenfei.huang@gdkyit.com>
2017-11-02 11:13:54 +0800

    dorado

---
+++ src/com/search/demo/entity/Branch.java
@@ -0,0 +1,23 @@
+package com.search.demo.entity;
+
+public class Branch {
+
+	int id;
+	String branchname;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBranchname() {
+		return branchname;
+	}
+	public void setBranchname(String branchname) {
+		this.branchname = branchname;
+	}
+	
+	
+	
+}
---
+++ src/com/search/demo/entity/Role.java
@@ -0,0 +1,28 @@
+package com.search.demo.entity;
+
+public class Role {
+	
+	int id;
+	String rolename;
+	String descriptionString;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRolename() {
+		return rolename;
+	}
+	public void setRolename(String rolename) {
+		this.rolename = rolename;
+	}
+	public String getDescriptionString() {
+		return descriptionString;
+	}
+	public void setDescriptionString(String descriptionString) {
+		this.descriptionString = descriptionString;
+	}
+	
+	
+}
